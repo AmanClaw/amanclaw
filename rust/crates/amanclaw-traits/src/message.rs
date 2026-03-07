f4b9f7d816a5a932a6da8b753058b421a03381a4
abdul rahman <amanasmuei@gmail.com>
2026-03-08 07:15:46 +0800

    feat(traits): add IncomingMessage and OutgoingMessage types

---
+++ rust/crates/amanclaw-traits/src/message.rs
@@ -0,0 +1,75 @@
+use serde::{Deserialize, Serialize};
+
+/// Normalized incoming message from any platform.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct IncomingMessage {
+    pub user_id: String,
+    pub chat_id: String,
+    pub platform: String,
+    pub text: String,
+    pub username: Option<String>,
+    pub first_name: Option<String>,
+    pub is_group: bool,
+    pub image_data: Option<Vec<u8>>,
+    pub reply_to: Option<String>,
+}
+
+/// Normalized outgoing message to any platform.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct OutgoingMessage {
+    pub chat_id: String,
+    pub text: String,
+    pub parse_mode: Option<String>,
+    pub reply_to: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_incoming_message_creation() {
+        let msg = IncomingMessage {
+            user_id: "12345".into(),
+            chat_id: "12345".into(),
+            platform: "telegram".into(),
+            text: "Hello bot".into(),
+            username: Some("aman".into()),
+            first_name: Some("Aman".into()),
+            is_group: false,
+            image_data: None,
+            reply_to: None,
+        };
+        assert_eq!(msg.platform, "telegram");
+        assert_eq!(msg.text, "Hello bot");
+    }
+
+    #[test]
+    fn test_outgoing_message_creation() {
+        let msg = OutgoingMessage {
+            chat_id: "12345".into(),
+            text: "Hi there!".into(),
+            parse_mode: None,
+            reply_to: None,
+        };
+        assert_eq!(msg.text, "Hi there!");
+    }
+
+    #[test]
+    fn test_incoming_message_serialization() {
+        let msg = IncomingMessage {
+            user_id: "12345".into(),
+            chat_id: "12345".into(),
+            platform: "telegram".into(),
+            text: "test".into(),
+            username: None,
+            first_name: None,
+            is_group: false,
+            image_data: None,
+            reply_to: None,
+        };
+        let json = serde_json::to_string(&msg).unwrap();
+        let deserialized: IncomingMessage = serde_json::from_str(&json).unwrap();
+        assert_eq!(deserialized.user_id, "12345");
+    }
+}
