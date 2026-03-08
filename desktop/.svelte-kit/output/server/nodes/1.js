b9d6899a32525768004b1899603622e6183cecf0
abdul rahman <amanasmuei@gmail.com>
2026-03-08 12:19:42 +0800

    chore: gitignore SvelteKit build artifacts

--- desktop/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CUYLDVki.js","_app/immutable/chunks/DyTrK3wT.js","_app/immutable/chunks/DQ-cvAFX.js","_app/immutable/chunks/CGLJJkVN.js","_app/immutable/chunks/jhvywJ_D.js","_app/immutable/chunks/B2xRBW8J.js"];
-export const stylesheets = [];
-export const fonts = [];
--- desktop/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.nrvW_TWf.js","_app/immutable/chunks/DyTrK3wT.js","_app/immutable/chunks/DQ-cvAFX.js","_app/immutable/chunks/CGLJJkVN.js","_app/immutable/chunks/B2xRBW8J.js","_app/immutable/chunks/DwfbzHCR.js","_app/immutable/chunks/s748XA2O.js","_app/immutable/chunks/D5cvNfD_.js"];
-export const stylesheets = [];
-export const fonts = [];
